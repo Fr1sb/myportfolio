3b541ef8b8441a6a5c703a5eaf4311679b46e6bb
TripleS <king_fr1sb@mail.ru>
2025-06-07 03:16:52 +0500

    weather

--- src/components/section/Projects.jsx
+++ src/components/section/Projects.jsx
@@ -61,10 +61,10 @@ export const Projects = () => {
               </div>
             </div>
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition-all">
-              <h3 className="text-xl font-bold mb-2">In developing</h3>
-              <p className="text-gray-400 mb-4">In developing</p>
+              <h3 className="text-xl font-bold mb-2">Weather App</h3>
+              <p className="text-gray-400 mb-4">Site uses the OpenWeather API for showing the local weather and 5 day forecast</p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Technology", "Technology", "Technology", "Technology"].map(
+                {["TypeScript", "Tailwindcss", "API", "ShadcnUI"].map(
                   (tech, key) => (
                     <span
                       key={key}
